17b4feab19270da2e7ebd23714efc39a1682977c
faroz206 <37172083+faroz206@users.noreply.github.com>
2018-04-05 04:16:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-// This isn't necessary but it keeps the editor from thinking L and carto are typos
-/* global L, carto */
 
 var map = L.map('map').setView([36.173357,-120.585937], 7);
 
-// Add base layer
+
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png', {
   maxZoom: 18
 }).addTo(map);
-// Initialize Carto
+
 var client = new carto.Client({
   apiKey: '624f9f417b56c45f823a89da9ca8de39f32202c6',
   username: 'faroz206'
 });
 
-// Initialze source data
+
 
 var source = new carto.source.SQL('SELECT *FROM cpad_2015');
 
-// Create style for the data
+
 var style = new carto.style.CartoCSS(`
 #layer {
   polygon-fill: ramp([access_typ], (#b6ffdf, #ffc400, #ff2424, #cfcfcf), ("Open Access", "Restricted Access", "No Public Access", "Unknown Access"), "=");
